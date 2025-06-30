62b46edde02a30d6454197e78494dced5cb263be
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-07-01 01:44:27 +0530

    feat: added mainLayout when user logs in/ logged out (BOOK and Events pages)

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,20 @@
+import PublicNavbar from "@/components/PublicNavBar";
+import PrivateNavbar from "@/components/PrivateNavBar";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function MainLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const user = await currentUser();
+	return (
+		<main className="relative">
+			{/* Rendering private navbar if user signed in else public navbar */}
+			{user ? <PrivateNavbar /> : <PublicNavbar />}
+
+			{/* Render the children */}
+			<section className="pt-36">{children}</section>
+		</main>
+	);
+}
