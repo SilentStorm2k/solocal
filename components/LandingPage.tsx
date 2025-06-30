ad298b4bfa0de7dea8a34bf10607d10df3afa720
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-07-01 01:45:13 +0530

    feat, fix: basic book, events page and fix to landing page styling

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function LandingPage() {
 	return (
-		<main className="flex items-center p-10 gap-24 animate-fade-in max-md:flex-col">
+		<main className="flex justify-center items-center p-10 gap-24 animate-fade-in max-md:flex-col">
 			<section className="flex flex-col items-center gap-10">
 				<Image
 					src={"/assets/logo.svg"}
