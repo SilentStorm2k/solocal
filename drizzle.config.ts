02824090d69ae419318bf6f7228bcc6db697de3f
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-07-01 00:29:27 +0530

    feat: add database configuration and schema definitions with enums for scheduling

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl)
+	throw new Error("DATABASE_URL not defined in environment variables");
+
+export default defineConfig({
+	out: "./drizzle/migrations", // where drizzle with output migration files
+	schema: "./drizzle/schema.ts",
+	dialect: "postgresql",
+	strict: true,
+	verbose: true,
+	dbCredentials: {
+		url: databaseUrl,
+	},
+});
---
+++ drizzle/db.ts
@@ -0,0 +1,9 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+// Initializing the Neon client using the Database_URL from env variables
+const sql = neon(process.env.DATABASE_URL!);
+
+// Creating and export the Drizzle ORM instance, with neon client and schema for type-safe queries
+export const db = drizzle(sql, { schema });
